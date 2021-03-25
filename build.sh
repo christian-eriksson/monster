cda3596e8815e4694a4d268c2f68b07436a15b93
Christian Eriksson <christian.eriksson@live.se>
2021-03-26 00:15:21 +0100

    Style radio buttons
    Radio buttons should pop when checked and you should notice when you
    hover them and they are active, both in checked and unchecked state.
    
    Also first time we use svg assets to style components so we add them
    to the build script.

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 COMPILE_DIR=compiled-theme
 GTK3_DIR=gtk-3.0
 GTK2_DIR=gtk-2.0
+ASSETS_DIR=assets
+
 mkdir -p $COMPILE_DIR/$GTK3_DIR
 sassc -t compact $GTK3_DIR/main.scss $COMPILE_DIR/$GTK3_DIR/gtk.css
 cp -r $GTK2_DIR $COMPILE_DIR
+cp -r $ASSETS_DIR $COMPILE_DIR
 
