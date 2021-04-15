c0caee98d3c0f2bb838117c17a8615cfcefc67b6
Christian Eriksson <christian.eriksson@live.se>
2021-04-15 23:43:03 +0200

    Add directory for application specific themes
    There are applications that may need individual styling to look the
    part.

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ COMPILE_DIR=compiled-theme
 GTK3_DIR=gtk-3.0
 GTK2_DIR=gtk-2.0
 ASSETS_DIR=assets
+APPLICATION_THEMES_DIR=application-theme
 
 mkdir -p $COMPILE_DIR/gtk/$GTK3_DIR/
 sassc -t compact $GTK3_DIR/main.scss $COMPILE_DIR/gtk/$GTK3_DIR/gtk.css
 cp -r $GTK2_DIR $COMPILE_DIR/gtk
 cp -r $ASSETS_DIR $COMPILE_DIR/gtk
+cp -r $APPLICATION_THEMES_DIR $COMPILE_DIR
