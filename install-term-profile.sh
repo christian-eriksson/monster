3e8c0de4b5ca59db0612bd41140cfe9bc6c4316c
cerfe5 <christian.eriksson@scania.com>
2021-07-15 17:48:53 +0200

    Install term profile correctly if no previous profile
    The install script for the terminal theme crashed if there was no previous
    profile installed. We need to check this in the script.

--- install-term-profile.sh
+++ install-term-profile.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
+set -e
+
 dconf_path="/org/gnome/terminal/legacy/profiles:"
 profile_path="compiled-theme/application-themes/terminal/gnome-terminal-profile.dconf"
 uuid=$(cat compiled-theme/application-themes/terminal/theme-uuid)
 installed_profiles=$(dconf read /org/gnome/terminal/legacy/profiles:/list)
 installed_profiles=${installed_profiles%?}
 
-monster_installed=$(echo "${installed_profiles}" | grep $uuid)
+[ -n "${installed_profiles}" ] && monster_installed=$(echo "${installed_profiles}" | grep $uuid)
 
-[ -z "${monster_installed}" ] && dconf write "${dconf_path}/list"  "${installed_profiles}, '${uuid}']"
+if [ -z "${monster_installed}" ]; then
+  if [ -z "${installed_profiles}" ]; then
+    dconf write "${dconf_path}/list"  "['${uuid}']"
+  else
+    dconf write "${dconf_path}/list"  "${installed_profiles}, '${uuid}']"
+  fi
+fi
 
 dconf write "${dconf_path}/default"  "'${uuid}'"
 
 dconf load "${dconf_path}/:${uuid}/" < $profile_path
+
+echo "term theme installed"
+
